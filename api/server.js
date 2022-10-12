08e5f864c801e597653aaa5bf958efc575b5389d
John Francis Bonifacio <johnfrancis.bonifacio@yahoo.com.ph>
2022-10-13 00:22:00 +0800

    added (1) end points for testing

--- api/server.js
+++ api/server.js
@@ -11,4 +11,27 @@ app.get("/", (req, res) => {
     res.json(Heroes)
 });
 
+let corsOptions = {
+    origin: 'http://localhost:3000'
+};
+
+app.get('/heroes', cors(corsOptions), function (req, res) {
+    const { q } = req.query;
+
+    const keys = ["localized_name"];
+
+    const search = (data) => {
+        return data.filter((item) =>
+            keys.some((key) => item[key].toLowerCase().includes(q))
+        );
+    };
+
+    // Sort localized_name A-Z
+    const heroes = [...Heroes].sort((a, b) =>
+        a.localized_name > b.localized_name ? 1 : -1,
+    );
+
+    q ? res.json(search(heroes).slice(0, 10)) : res.json(heroes);
+});
+
 app.listen(PORT_NUMBER, () => console.log('Api server running at port: ' + PORT_NUMBER));
