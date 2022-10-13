e638ac856d0d4e53fdca78ca706e97cf12db39b2
John Francis Bonifacio <johnfrancis.bonifacio@yahoo.com.ph>
2022-10-14 06:14:37 +0800

    filter cursor pointer

--- client/src/components/filter.component/Filter.jsx
+++ client/src/components/filter.component/Filter.jsx
@@ -3,10 +3,9 @@ import "./filter.css";
 const FILTER_URL = 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/herogrid/filter-';
 const ICON_URL = "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_";
 
-const Filter = ({styleCss, value, onClick}) => (
+const Filter = ({classCss ,styleCss, value, onClick}) => (
     <img
-        className="filter-item"
-        style={styleCss}
+        className={classCss}
         src={ICON_URL + value}
         onClick={(event) => onClick(event)}
     />
