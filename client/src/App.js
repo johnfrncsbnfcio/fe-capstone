3048510aa8b1956ecee3a65e8890b8c4af41529b
John Francis Bonifacio <johnfrancis.bonifacio@yahoo.com.ph>
2022-10-14 21:59:09 +0800

    modular component, removed old component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,41 @@
-import React from "react";
-import Gallery from "./components/gallery.component/Gallery";
-import './App.css';
+import React, { useReducer } from 'react'
+import Galleries from './components/galleries.component/Galleries'
+import Controller from './components/controller.component/Controller'
+import './App.css'
 
-// test
+export const GalleryContext = React.createContext()
 
-const App = () => {
+export const INIT_REQUEST = 'init request';
 
-	return (
-		<div id="container">
-			<Gallery />			
-		</div>
-	);
+const initialState = {
+	searchedInput: '',
+	selectedAttr: ''
 }
 
-export default App;
+const reducer = (state, action) => {
+	switch (action.type) {
+		case INIT_REQUEST:
+			return {
+				...state,
+				[action.key]: action.value
+			}
+		default:
+			return state
+	}
+}
+
+const App = () => {
+	const [requestState, dispatch] = useReducer(reducer, initialState);
+	return (
+		<div id="container">
+			<GalleryContext.Provider
+				value={{ queryState: requestState, queryDispatch: dispatch }}>
+				<Controller />
+				<Galleries />
+			</GalleryContext.Provider>
+		</div>
+
+	)
+}
+
+export default App
